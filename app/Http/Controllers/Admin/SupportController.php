ccfcbbc885f27de67140090c00bd8b821af8f581
Kaique Nocetti <kaiquenocetti07@gmail.com>
2023-08-10 00:10:04 -0300

    Nova aula

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -20,8 +20,21 @@ class SupportController extends Controller
         return view('admin/supports/create');
     }
 
-    public function store(Request $request)
+    //public function store(Request $request) DUAS FORMAS DE FAZER
+    public function store(Request $request, Support $support)
     {
-        dd($request->all());
+        //dd($request->body); PEGA O VALOR DO CAMPO DE FORMA SIMPLIFICADA
+        //dd($request->get('axas', 'default')); se campo não existir define valor como default
+        //dd($request->get('body'));
+
+        $data = $request->all(); // PEGA TODOS OS DADOS DO FORMULÁRIO
+        $data['status'] = 'a'; // DEFINE O VALOR STATUS COMO 'A'
+
+        //Support::create($data); // Cadastro os valores na base de dados com os dados passados na variavel
+       // $support->create($data);
+        $support = $support->create($data);
+        //dd($support); valida 
+
+        return redirect()->route('supports.index');
     }
 }
--- app/Observers/SupportObserver.php
+++ app/Observers/SupportObserver.php
@@ -7,11 +7,10 @@ use Illuminate\Support\Facades\Auth;
 
 class SupportObserver
 {
-    /**
-     * Handle the Support "creating" event.
-     */
+    /*Handle the Support "creating" event.
+    
     public function creating(Support $support): void
     {
         $support->user_id = Auth::user()->id;
-    }
+    } */
 }
