c98ce2eefe1f009cd8e20f91c5ef6ee861b1f791
Kaique Nocetti <kaiquenocetti07@gmail.com>
2023-08-23 23:35:52 -0300

    API REST aula 31

---
+++ app/Http/Controllers/Api/SupportController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\DTO\Supports\CreateSupportDTO;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
+use App\Http\Resources\SupportResource;
+use App\Services\SupportService;
+use Illuminate\Http\Request;
+
+class SupportController extends Controller
+{
+    public function __construct(
+        protected SupportService $service,
+    ){}
+
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUpdateSupport $request)
+    {
+        $support = $this->service->create(
+            CreateSupportDTO::makeFromResquest($request)
+        );
+
+        return new SupportResource($support);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        if(!$support = $this->service->findOne($id)){
+            return response()->json([
+                'error' => 'Not found'
+            ], 404);
+        };
+
+        return new SupportResource($support);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        if(!$this->service->findOne($id)){
+            return response()->json([
+                'error' => 'Not found'
+            ], 404);
+        };
+
+        $this->service->delete($id);
+        return response()->json([], 204);
+    }
+}
